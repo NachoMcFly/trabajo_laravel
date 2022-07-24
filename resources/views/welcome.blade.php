d9e1960f62f3bdeae6e464f00e6f8b2393914937
Ignacio Moraga <ignacio.moraga.pineda@ciisa.cl>
2022-07-24 18:15:41 -0400

    imagenes patines

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,8 +29,9 @@
              <p>
 
  <div class="row">
-             <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <div class="card" style="width: 18rem;">
+  <img src="https://cdn.shopify.com/s/files/1/1629/6545/products/1_4_1024x1024_2x_5f33b371-f676-4c1e-a1b7-deffdc3f186f_1024x.jpg?v=1630522669" 
+             class="card-img-top" alt="..." width="190" height="210">
   <div class="card-body">
     <h5 class="card-title">Patines Seba</h5>
     <p class="card-text">Catalogados como la mejor marca de patines profesionales.</p>
@@ -41,7 +42,8 @@
 
 
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+<img src="https://cdn.shopify.com/s/files/1/1629/6545/products/IMG_0016_1_1x1_1_1200_x_1200_500x.jpg?v=1628525938" 
+             class="card-img-top" alt="..." width="190" height="210">
   <div class="card-body">
     <h5 class="card-title">Patines hook</h5>
     <p class="card-text">Patins semi profesionales, ideales para aprender.</p>
@@ -52,7 +54,8 @@
 
 
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+<img src="https://cdn.shopify.com/s/files/1/1629/6545/products/HD_INLINENEGRO_3_500x.jpg?v=1604279254" 
+             class="card-img-top" alt="..." width="190" height="210">
   <div class="card-body">
     <h5 class="card-title">Flying Eagle</h5>
     <p class="card-text">Patines profesionales pre rockeables.</p>
